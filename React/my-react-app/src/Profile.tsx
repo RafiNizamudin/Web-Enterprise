f10385d63aa713bef8de1c413c1054b6576662c7
RafiNizamudin <rafi.nizamudin01@gmail.com>
2025-04-23 11:23:18 +0700

    upload

---
+++ React/my-react-app/src/Profile.tsx
@@ -0,0 +1,16 @@
+
+function Profile() {
+
+    return (
+      <>    
+            <h1>
+                Rafi Nizamudin
+            </h1>    
+            <h2 className="bio">
+                Mahasiswa Universitas Mercu Buana
+            </h2>
+      </>
+    )
+  }
+  
+  export default Profile
