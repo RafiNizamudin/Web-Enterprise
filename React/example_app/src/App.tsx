73d9718478a327816e796f7e9f7dd9a0376a91c4
RafiNizamudin <rafi.nizamudin01@gmail.com>
2025-06-11 08:13:11 +0700

    Update

--- React/example_app/src/App.tsx
+++ React/example_app/src/App.tsx
@@ -1,49 +1,20 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import { useState } from 'react';
-import { TodoItem } from './components/TodoItem'
-import type { TodoItemProps } from './TodoItem.dto';
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, todo: 'Belajar React', status: 0, isDeleted: 0 },
-    { id: 2, todo: 'Belajar Project', status: 0, isDeleted: 0},
-    { id: 3, todo: 'Belajar Vite', status: 0, isDeleted: 0},
-    { id: 4, todo: 'Belajar JS', status: 0, isDeleted: 0},
-    { id: 5, todo: 'Belajar HTML', status: 0, isDeleted: 0},
-    { id: 6, todo: 'Belajar CSS', status: 0, isDeleted: 0},
-    { id: 7, todo: 'Belajar Java', status: 0, isDeleted: 0},
-    { id: 8, todo: 'Belajar C++', status: 0, isDeleted: 0},
-    { id: 9, todo: 'Belajar PHP', status: 0, isDeleted: 0},
-    { id: 10, todo: 'Belajar Python', status: 0, isDeleted: 0},
-  ] as TodoItemProps[]);
+const [counter, setCounter] = useState(1);
 
-  const onHandleMarkDone = (id: number) => {
-    setTodos(todos.map(todo =>
-      todo.id === id ? {...todo, status: 1} : todo
-    ))};
-
-  const onHandleDelete = (id: number) => {
-    setTodos(todos.map(todo =>
-      todo.id === id ? {...todo, isDeleted: 1} : todo
-    ))};
-
-  return (
-      <div>
-        <h1>Todo List</h1>
-        <button onClick={() => {}}>+ Add Todo</button>
-
-        <ul>
-          { todos 
-            .filter(todo => todo.isDeleted === 0)
-            .map(todo => (
-              <TodoItem
-                key={todo.id}
-                {...todo}
-                onMarkDone={onHandleMarkDone}
-                onDelete={onHandleDelete}
-              />
-            ))}
-        </ul>
-      </div>
-  );
+  useEffect(() => {
+    console.log('UseEffect Called');
+    return () => {
+      console.log('Cleanup function Called');
+    }
+  }, [counter]);
+  
+  return(
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter+1)}>Tambah</button>
+    </>
+  )
 }
--- React/todo-list-app/src/App.tsx
+++ React/todo-list-app/src/App.tsx
@@ -6,13 +6,16 @@ import supabase from './utils/supabase';
 
 export default function App() {
   const [todos, setTodos] = useState<todoDto[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [newTodo, setNewTodo] = useState('');
+
 
   useEffect(() => {
-    async function getTodos() {
-      
-      const { data: dataTodos, error } = await supabase.from('todos').select();
+    async function getTodos() {    
+      const { data: dataTodos, error } = await supabase
+      .from('todos')
+      .select();
 
-      console.log('data todos', dataTodos)
       if (error) {
         console.log('error', error);
       }
@@ -20,26 +23,55 @@ export default function App() {
         setTodos(dataTodos)
       }
     }
-
     getTodos();
   }, [])
 
-  const onHandleMarkDone = (id: number) => {
-    setTodos(todos.map(todo =>
-      todo.id === id ? {...todo, status: 1} : todo
-    ));
+  const onHandleMarkDone = async (id: number) => {
+    await supabase.from('todos').update({ status: 1}).eq('id', id);
+    setTodos(todos.map(todo => (todo.id === id ? {...todo, status: 1} : todo)));
   };
 
-  const onHandleDelete = (id: number) => {
-    setTodos(todos.map(todo =>
-      todo.id === id ? {...todo, is_deleted: 1} : todo
-    ));
+  const onHandleDelete = async (id: number) => {
+    await supabase.from('todos').update({ is_deleted: 1}).eq('id', id);
+    setTodos(todos.map(todo => (todo.id === id ? {...todo, is_deleted: 1} : todo)));
+  };
+
+  const onHandleAddTodo = async (todo: string) => {
+    if (todo.trim() === '') {
+      alert('Todo tidak boleh kosong');
+      return;
+    }
+
+    const { data, error } = await supabase
+    .from('todos')
+    .insert({ todo: todo, status: 0, is_deleted: 0})
+    .select();
+
+    if (!error && data) {
+      setTodos([...todos, data[0]]);
+      setShowModal(false);
+      setNewTodo('');
+    }
   };
 
   return (
       <div>
         <h1>Todo List</h1>
-        <button onClick={() => {}}>+ Add Todo</button>
+        <button onClick={() => setShowModal(true)}>+ Add Todo</button>
+        { showModal && (
+          <div className='modal'>
+            <div className='modal-content'>
+              <input 
+                type="text" 
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder='Masukan todo'
+              /><br/>
+              <button onClick={() => onHandleAddTodo(newTodo)}>Simpan</button>
+              <button onClick={() => setShowModal(false)}>Batal</button>
+            </div>
+          </div>
+        )}
         <ul>
           { todos 
             .filter(todo => todo.is_deleted === 0)
