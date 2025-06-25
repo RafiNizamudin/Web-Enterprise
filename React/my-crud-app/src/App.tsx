52d5eacd2fccf299eb26cea01f82ba6f2692df36
RafiNizamudin <rafi.nizamudin01@gmail.com>
2025-06-25 10:05:24 +0700

    update

---
+++ React/my-crud-app/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import UserRegister from './pages/UserRegister'
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/register' element={<UserRegister />} />
+        </Route>  
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
